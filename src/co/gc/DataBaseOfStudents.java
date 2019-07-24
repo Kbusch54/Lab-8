3f1ba82d9c585b5a3226b160a5bd313d96248854
Kbusch54 <kevinbusch@comcast.net>
2019-07-24 10:20:04 -0400

    Fixed evrything

--- src/co/gc/DataBaseOfStudents.java
+++ src/co/gc/DataBaseOfStudents.java
@@ -3,7 +3,7 @@ package co.gc;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class StudentDatabase {
+public class DataBaseOfStudents {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -16,35 +16,40 @@ public class StudentDatabase {
 
 		System.out.println(
 				"Welcome to our Java Class. Which student would you like to learn more about? (enter a number 1 - 10): ");
-		boolean cont =true;
-		while(cont) {
+	
+		boolean cont = true;
+		do {
 			
 		String choice1;
 		String choice2;
-		int numInput;
+		int numInput =0;
 		numInput = scan.nextInt();
 		scan.nextLine();
 		int i = (numInput - 1);
-		boolean cont4 = true;
-		while(cont4) {
+		
+		
 		
 		if (numInput > 10 || numInput < 1) {
 			System.out.println("The student does not exist. Please try again. (enter a number 1 - 10): ");
-			numInput=scan.nextInt();
 			
+			numInput = 0;
+			
+		
 		} else if(numInput>0 && numInput <=10){
 			System.out.println("Student " + numInput + " is " + names[i] + ". What would you like to know about "
 					+ names[i] + "? (enter \"hometown\" or \"favorite food\"): "); 
-			cont4=false;
-			cont=false;
+			
+			
 		}
+		
 		else {System.out.println("Enter a whole number");}
+		
 		choice1 = scan.nextLine();
 		boolean cont3 =true;
 		if (choice1.equalsIgnoreCase("hometown")) {
 			System.out.println(
 					names[i] + " is from " + homeT[i] + ". Would you like to know more? (enter \"yes\" or \"no\"):");
-			while(cont3) {
+			do {
 			choice2 = scan.nextLine();
 			if (choice2.equalsIgnoreCase("yes")) {
 				System.out.println(names[i] + " favorite food is " + food[i]);
@@ -56,36 +61,44 @@ public class StudentDatabase {
 			} else {
 				System.out.println("That data does not exist. Please try again. (enter \"yes\" or \"no\"): ");
 			}
-		}
+		}while(cont3);
 		
 		
 	}	else if (choice1.equalsIgnoreCase("favorite food")) {
 			System.out.println(names[i] + "favorite food is " + food[i]
 					+ ". Would you like to know more? (enter \"yes\" or \"no\"):");
-			choice2 = scan.nextLine();
 			boolean cont2 = true;
-			while(cont2)
+			do {
+				choice2 = scan.nextLine();
 			if (choice2.equalsIgnoreCase("yes")) {
 				System.out.println(names[i] + "is from " + homeT[i]);
 				cont2 = false;
 				cont =false;
+				
 			} else if (choice2.equalsIgnoreCase("no")) {
-				cont = false;
+				
 				cont2 = false;
+				cont =false;
 			} else {
 				System.out.println("That data does not exist. Please try again. (enter \"yes\" or \"no\"): ");
-				choice2 = scan.nextLine();
+			
 			}
+			}while(cont2);
 		} else {
 
 			System.out.println(
-					"The data does not exist heres a list of student to choose from");
+					"The data does not exist here is a list of student to choose from pick a number from 1 -10!");
 			System.out.println(Arrays.toString(names));
 			
+			
 		}
-		}
+		}while(cont);
 		System.out.println("Thanks!");
 		scan.close();
 	}
 }
-}
+
+
+	
+
+
